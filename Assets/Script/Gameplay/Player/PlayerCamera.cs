743bb85b1ea4d330248d48d54655f6de629d9899
Guillaume LM <Matrix0150@gmail.com>
2019-04-27 20:06:34 -0400

    New Files

---
+++ Assets/Script/Gameplay/Player/PlayerCamera.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using CleverCode;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] private Transform _pole;
+    [SerializeField] private Transform _pivot;
+
+    [Range(0f, 1f)]
+    [SerializeField] private float _distanceLerp;
+
+    private float _currentDistance;
+    private float distance
+    {
+        get { return _currentDistance; }
+        set
+        {
+            _pivot.transform.localPosition = Vector3.back * value;
+            _currentDistance = value;
+        }
+    }
+
+    [NonSerialized]
+    public float targetDistance;
+
+    public void Update()
+    {
+        CheckMaxAngle();
+
+        UpdateDistance();
+    }
+
+    private void CheckMaxAngle()
+    {
+        //TO BE DONE
+        //raycast to make sure the camera can go low enough without going through the ground
+        //find a valid angle
+    }
+
+    private void UpdateDistance()
+    {
+        float currentDist = _currentDistance;
+        float targetDist = targetDistance;
+
+        float lerp = 1 - Mathf.Pow(1 - _distanceLerp, Time.deltaTime * 10);
+
+        distance = Mathf.Lerp(currentDist, targetDist, lerp);
+    }
+
+    public Vector3 GetForward()
+    {
+        return _pivot.forward;
+    }
+
+    public Vector3 GetRight()
+    {
+        return _pivot.right;
+    }
+}
---
+++ Assets/Script/Manager/CheatManager.cs
@@ -0,0 +1,23 @@
+﻿using CleverCode;
+using UnityEngine;
+
+public class CheatManager : Singleton<CheatManager>
+{
+    private void Update()
+    {
+        CheckTimeCheat(KeyCode.Alpha1, 0.0f);
+        CheckTimeCheat(KeyCode.Alpha2, 0.1f);
+        CheckTimeCheat(KeyCode.Alpha3, 0.4f);
+        CheckTimeCheat(KeyCode.Alpha4, 1.0f);
+        CheckTimeCheat(KeyCode.Alpha5, 2.0f);
+        CheckTimeCheat(KeyCode.Alpha6, 5.0f);
+    }
+
+    private void CheckTimeCheat(KeyCode keyCode, float scale)
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+            Time.timeScale = scale;
+        }
+    }
+}
