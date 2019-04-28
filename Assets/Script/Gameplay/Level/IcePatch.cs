b7c7e43e0e2bc0c2649cc18829753eb06fffe8ac
Belug <alexandre.lessard.ti@gmail.com>
2019-04-28 17:25:47 -0400

    Removed Debug code

--- Assets/Script/Gameplay/Level/IcePatch.cs
+++ Assets/Script/Gameplay/Level/IcePatch.cs
@@ -7,31 +7,19 @@ public class IcePatch : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("In");
         if (other.gameObject.layer == Layer.Ball)
         {
-            Debug.Log("Payler found");
             Player player = other.transform.parent.gameObject.GetComponent<Player>();
             player.OnIce = true;
-        }
-        else
-        {
-            Debug.Log(other.GetType());
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("Out");
         if (other.gameObject.layer == Layer.Ball)
         {
-            Debug.Log("Payler found");
             Player player = other.transform.parent.gameObject.GetComponent<Player>();
             player.OnIce = false;
-        }
-        else
-        {
-            Debug.Log(other.GetType());
         }
     }
 }
