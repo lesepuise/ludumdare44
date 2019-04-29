ce5af85d50845e7ee112d5f1b971b910622482df
Franz <elfteiroh@hotmail.com>
2019-04-28 20:56:47 -0400

    [UI] Started touching up the shop visuals [Animation] The UFO now rotate

---
+++ Assets/RotateSelf.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSelf : MonoBehaviour
+{
+    public enum Axis
+    {
+        None = 0,
+        Up,
+        Down,
+        Back,
+        Front,
+        Left,
+        Right,
+    }
+
+    public Axis RotationAxis = Axis.None;
+    public float AngleSpeed = 1;
+
+    private void FixedUpdate()
+    {
+        switch(RotationAxis)
+        {
+            case Axis.Up:
+                transform.Rotate(Vector3.up, AngleSpeed);
+                break;
+            case Axis.Down:
+                transform.Rotate(Vector3.down, AngleSpeed);
+                break;
+            case Axis.Back:
+                transform.Rotate(Vector3.back, AngleSpeed);
+                break;
+            case Axis.Front:
+                transform.Rotate(Vector3.forward, AngleSpeed);
+                break;
+            case Axis.Left:
+                transform.Rotate(Vector3.left, AngleSpeed);
+                break;
+            case Axis.Right:
+                transform.Rotate(Vector3.right, AngleSpeed);
+                break;
+            default:
+                break;
+        }
+    }
+}
