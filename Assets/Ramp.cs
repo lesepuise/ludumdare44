07bcdce3d2a4dbb0ebf489f8774de4e726c4917f
Guillaume LM <Matrix0150@gmail.com>
2019-04-29 20:57:04 -0400

    Ramps Power

---
+++ Assets/Ramp.cs
@@ -0,0 +1,19 @@
+﻿using CleverCode;
+using UnityEngine;
+
+public class Ramp : MonoBehaviour
+{
+    public Transform velocityTarget;
+
+    public float strength;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == Layer.Ball)
+        {
+            Player player = other.transform.parent.gameObject.GetComponent<Player>();
+            
+            player.RigidBody.AddForce(velocityTarget.forward * strength, ForceMode.Acceleration);
+        }
+    }
+}
--- Assets/RampSpot.cs
+++ Assets/RampSpot.cs
@@ -1,8 +1,5 @@
-﻿
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using CleverCode;
 
 public class RampSpot : MonoBehaviour
 {
